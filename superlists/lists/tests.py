df273243df3b7ff1021b98682464db367551934a
kitty88825 <kitty40632@gmail.com>
2021-02-28 17:27:13 +0800

    ch6 p92 綠燈？重構

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,26 +1,21 @@
 from django.test import TestCase
-from django.urls import resolve
 
 from .models import Item
-from .views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_page_can_save_a_POST_request(self):
+    def test_can_save_a_POST_request(self):
         self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-    def test_home_page_redirects_after_POST(self):
+    def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(
@@ -28,19 +23,13 @@ class HomePageTest(TestCase):
             '/list/the-only-list-in-the-world/',
         )
 
-    def test_home_page_only_saves_items_when_necessary(self):
+    def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        response = self.client.get('/')
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
-
 
 class ItemModelTest(TestCase):
+
     def test_saving_and_retrieving_items(self):
         first_item = Item()
         first_item.text = 'The first (ever) list item'
@@ -60,6 +49,7 @@ class ItemModelTest(TestCase):
 
 
 class ListViewTest(TestCase):
+
     def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
