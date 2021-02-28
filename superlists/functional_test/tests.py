5b155df4d21b4c9451e979ebf2b551b7a250a06e
kitty88825 <kitty40632@gmail.com>
2021-02-28 15:32:56 +0800

    ch6 p80 確保多個功能測試不會互相影響

--- superlists/functional_test/tests.py
+++ superlists/functional_test/tests.py
@@ -1,11 +1,12 @@
 import time
-import unittest
+
+from django.test import LiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):  # 測試前執行
         self.browser = webdriver.Firefox(executable_path='/tmp/geckodriver')
         self.browser.implicitly_wait(3)
@@ -21,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith 聽到一個很酷的新線上待辦事項app
         # 他去查看他的首頁
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # 他發現網頁標題與標題顯示待辦事項清單
         self.assertIn('To-Do', self.browser.title)
@@ -64,7 +65,3 @@ class NewVisitorTest(unittest.TestCase):
 
         # 他前往那個 URL 他的待辦事項仍然在那裡
         # 他很滿意地上床睡覺
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
-
-browser.quit()
