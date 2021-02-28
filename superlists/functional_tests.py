871670c834a8f9e433b13837eb72fbeef3645aec
kitty88825 <kitty40632@gmail.com>
2021-02-28 13:53:10 +0800

    ch5 p56 將要顯示的 Python 變數傳入範本

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -40,18 +40,27 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table",
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         # 此時仍然有一個文字方塊，讓他可以加入另一個項目
         # 他輸入 "使用孔雀羽毛來製作一隻蒼蠅" (Edith 非常有條理)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # 網頁再次更新，現在他的清單有這兩個項目
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows],
+        )
         # Edith 不知道網站能否記得他的清單
         # 接著他看到網站產生一個唯一的 URL 給他
         # 網頁有一些文字說明這個效果
+        self.fail('Finish the test!')
+
         # 他前往那個 URL 他的待辦事項仍然在那裡
         # 他很滿意地上床睡覺
 if __name__ == '__main__':
