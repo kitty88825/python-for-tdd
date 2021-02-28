e86843316cc7f95d2c62ae45fb25e089dc6f3e48
kitty88825 <kitty40632@gmail.com>
2021-02-28 14:36:32 +0800

    ch5 p66 將 POST 存至資料庫
    如果 method == 'GET'，則不做 Item 的 create

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -18,8 +18,17 @@ class HomePageTest(TestCase):
 
     def test_home_page_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ItemModelTest(TestCase):
