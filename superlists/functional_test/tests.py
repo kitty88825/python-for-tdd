407a4556384343283a34c54584005614374a2734
kitty88825 <kitty40632@gmail.com>
2021-02-28 23:35:57 +0800

    ch7 p120 哪些版面配置與樣式需要做功能測試？

--- superlists/functional_test/tests.py
+++ superlists/functional_test/tests.py
@@ -112,3 +112,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+    def test_layout_and_styling(self):
+        # Edith 前往首頁
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # 他發現輸入方塊已被妥善地置中
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10,
+        )
+
+        # 他開始編輯一個新清單，看到這裡的
+        # 輸入欄位也妥善地置中
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10,
+        )
