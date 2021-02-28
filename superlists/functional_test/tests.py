508ab632f9aa8bd5be608fc25694bd3e679b70dd
kitty88825 <kitty40632@gmail.com>
2021-02-28 16:23:20 +0800

    Revert "ch6 p85 使用 TDD 來進行新的設計"
    This reverts commit 2f2e2c5fd7ab59ba5955e6ea69c3403df414240f.

--- superlists/functional_test/tests.py
+++ superlists/functional_test/tests.py
@@ -39,13 +39,10 @@ class NewVisitorTest(LiveServerTestCase):
         # 他在文字方塊輸入"購買孔雀羽毛"
         # (Edith 的興趣是綁蒼蠅魚餌)
         inputbox.send_keys('Buy peacock feathers')
-        # 當他按下 enter 時，他會被帶到一個新的 URL
-        # 現在網頁列出 "1: Buy peacock feathers"
-        # 一個待辦事項清單項目
+        # 當他按下 enter 時，網頁會更新，現在網頁列出
+        # "1: 購買孔雀羽毛"，一個待辦事項清單項目
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/list/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 此時仍然有一個文字方塊，讓他可以加入另一個項目
@@ -61,33 +58,10 @@ class NewVisitorTest(LiveServerTestCase):
             '2: Use peacock feathers to make a fly',
         )
 
-        # 在新的使用者 Framcis 來到網站
+        # Edith 不知道網站能否記得他的清單
+        # 接著他看到網站產生一個唯一的 URL 給他
+        # 網頁有一些文字說明這個效果
+        self.fail('Finish the test!')
 
-        ## 我們使用一個新的瀏覽器工作階段來確保
-        ## Edith 的任何資訊都不會被 cookies 等機制送出
-        self.browser.quit()
-        self.browser = webdriver.Firefox(executable_path='/tmp/geckodriver')
-
-        # Francis 造訪首頁。沒有任何 Edith 的清單的跡象
-        self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertNotIn('make a fly', page_text)
-
-        # Francis 輸入一個新的項目，做出一個新的清單
-        # 他比 Edith 無趣...
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy milk')
-        inputbox.send_keys(Keys.ENTER)
-
-        # Francis 取得他自己的獨一無二 URL
-        francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, '/list/.+')
-        self.assertNotEqual(francis_list_url, edith_list_url)
-
-        # 同樣的，沒有 Edith 的清單的任何跡象
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertIn('Buy milk', page_text)
-
+        # 他前往那個 URL 他的待辦事項仍然在那裡
         # 他很滿意地上床睡覺
