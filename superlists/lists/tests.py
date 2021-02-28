25a75742fde65c519e23be923860f62b836fa4fe
kitty88825 <kitty40632@gmail.com>
2021-02-28 20:07:21 +0800

    ch6 p109 將項目加到既有清單的另一個 View (One More View to Handle Adding Items to an Existing List)

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -66,6 +66,31 @@ class ListViewTest(TestCase):
 
 class NewListTest(TestCase):
 
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'},
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'},
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
     def test_saving_a_POST_request(self):
         self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
