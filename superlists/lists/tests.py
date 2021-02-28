1275141a8b804e845cd23e327452bc50ad2b9ced
kitty88825 <kitty40632@gmail.com>
2021-02-28 13:04:33 +0800

    修正因 Django 版本大於1.8並且render_to_string並未生成CSRF token 導致測試失敗
    AssertionError: '<htm[207 chars]     <input type="hidden" name="csrfmiddleware[161 chars]l>\n' != '<htm[207 chars]     \n        </form>\n        <table id="id_[40 chars]l>\n'

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -12,7 +12,5 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
