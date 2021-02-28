5ab3c534615db55d7d059d868406381b493a50a5
kitty88825 <kitty40632@gmail.com>
2021-03-01 00:43:13 +0800

    ch7 p133 我們隱瞞了什麼：collectstatic 與其他靜態字典
    What We Glossed Over: collectstatic and Other Static Directories

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -120,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
