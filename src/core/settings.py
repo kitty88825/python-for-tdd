99fb92645464d1c8d2dbf0bc3f9eb10b15a3d911
kitty88825 <kitty40632@gmail.com>
2021-03-06 22:29:46 +0800

    修改專案名稱

--- src/core/settings.py
+++ src/core/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'superlists.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'superlists.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
