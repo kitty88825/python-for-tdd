cf9979cad1076eec9c57b871934bacb07fedb11e
Kitty Hsieh <jstykitty@it888.com>
2023-05-25 12:08:25 +0800

    Unit Testing a View

--- app/lists/tests.py
+++ app/lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
@@ -5,6 +6,14 @@ from app.lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
+    def test_root_url_resolves_to_home_page_view(self) -> None:
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self) -> None:
+        request = HttpRequest()
+        response = home_page(request)
+        html: str = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
