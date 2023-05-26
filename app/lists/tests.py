653a9885d3423eb82e7eb9b82057b4a19cd0469e
Kitty Hsieh <jstykitty@it888.com>
2023-05-26 14:09:50 +0800

    A Foreign Key Relationship

--- app/lists/tests.py
+++ app/lists/tests.py
@@ -46,7 +46,7 @@ class ListAndItemModelsTest(TestCase):
 
 class ListViewTest(TestCase):
 
-    def test_uses_list_template(self):
+    def test_uses_list_template(self) -> None:
         response = self.client.get('/lists/the-only-list-in-the-world')
         self.assertTemplateUsed(response, 'list.html')
 
