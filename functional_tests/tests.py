66db34334e3df1362d2fb1f81c071af7fef4f3b2
Kitty Hsieh <kitty40632@gmail.com>
2023-05-27 15:16:31 +0800

    Switching to StaticLiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,6 @@
 import time
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
@@ -11,7 +11,7 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self) -> None:
         super().setUp()
         self.browser = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
